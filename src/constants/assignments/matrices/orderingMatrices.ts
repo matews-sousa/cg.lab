e33fb4d1380ce33788457616bd3eb876e3e07c32
Mateus Sousa <mateussdamiano@gmail.com>
2025-03-17 22:44:52 -0300

    feat: add more matrix multiplication assignments

--- src/constants/assignments/matrices/orderingMatrices.ts
+++ src/constants/assignments/matrices/orderingMatrices.ts
@@ -1,3 +1,4 @@
+import { degreesToRadians } from "@/lib/utils";
 import {
   MatrixOption,
   useOrderMatrixStore,
@@ -32,7 +33,6 @@ function createOrderMatrixMultiplication({
     setup() {
       const { setPolygons, setObjectivePolygons } = useScene2DStore.getState();
       setPolygons(initialPolygons);
-
       setObjectivePolygons(objectivePolygons);
 
       const { createObject, setMatricesOptions } =
@@ -44,13 +44,13 @@ function createOrderMatrixMultiplication({
       const square1 = useScene2DStore.getState().getPolygon("square1");
       if (!square1) return false;
 
+      const objectiveSquare = objectivePolygons[0];
       return (
         square1.points.every(
           (point, index) =>
-            point.position[0] ===
-              objectivePolygons[0].points[index].position[0] &&
-            point.position[1] === objectivePolygons[0].points[index].position[1]
-        ) && square1.color === objectivePolygons[0].color
+            point.position[0] === objectiveSquare.points[index].position[0] &&
+            point.position[1] === objectiveSquare.points[index].position[1]
+        ) && square1.color === objectiveSquare.color
       );
     },
   };
@@ -70,6 +70,36 @@ function createSquare(
       position: point,
       movable: false,
     })),
+  };
+}
+
+function applyMatrixToSquare(matrices: Matrix3[], square: TPolygon): TPolygon {
+  const newPoints = square.points?.map(point => {
+    const modelMatrix = new Matrix3().identity();
+    matrices.forEach(matrix => modelMatrix.multiply(matrix));
+
+    const [x, y] = point.position;
+    const w = 1;
+    const newX =
+      modelMatrix.elements[0] * x +
+      modelMatrix.elements[1] * y +
+      modelMatrix.elements[2] * w;
+    const newY =
+      modelMatrix.elements[3] * x +
+      modelMatrix.elements[4] * y +
+      modelMatrix.elements[5] * w;
+    return {
+      id: point.id,
+      position: [newX, newY] as [number, number],
+      movable: false,
+    };
+  });
+
+  if (!newPoints) return square;
+
+  return {
+    ...square,
+    points: newPoints,
   };
 }
 
@@ -109,6 +139,90 @@ const orderingMatricesAssignmentsProps: OrderMatrixMultiplicationProps[] = [
       {
         id: "translate-back",
         matrix: new Matrix3().translate(1, 1).transpose(),
+      },
+    ],
+  },
+  {
+    order: 3,
+    initialPolygons: [createSquare("square1", "blue", [1, 1], [1, 1])],
+    objectivePolygons: [
+      applyMatrixToSquare(
+        [
+          new Matrix3().translate(-1, -1).transpose(),
+          new Matrix3().scale(2, 2),
+          new Matrix3().rotate(degreesToRadians(45)),
+        ],
+        createSquare("square1", "blue", [1, 1], [1, 1])
+      ),
+    ],
+    matricesOptions: [
+      {
+        id: "scale",
+        matrix: new Matrix3().scale(2, 2),
+      },
+      {
+        id: "rotation-z",
+        matrix: new Matrix3().rotate(degreesToRadians(45)),
+        rotationAxis: "z",
+        rotationAngle: 45,
+      },
+      {
+        id: "translate-to-origin",
+        matrix: new Matrix3().translate(-1, -1).transpose(),
+      },
+    ],
+  },
+  {
+    order: 4,
+    initialPolygons: [createSquare("square1", "blue", [0, 0], [2, 2])],
+    objectivePolygons: [
+      applyMatrixToSquare(
+        [
+          new Matrix3().scale(0.5, 0.5),
+          new Matrix3().translate(0.5, 0.5).transpose(),
+        ],
+        createSquare("square1", "blue", [0, 0], [2, 2])
+      ),
+    ],
+    matricesOptions: [
+      {
+        id: "translate-to-origin",
+        matrix: new Matrix3().translate(-1, -1).transpose(),
+      },
+      {
+        id: "translate",
+        matrix: new Matrix3().translate(0.5, 0.5).transpose(),
+      },
+      {
+        id: "scale",
+        matrix: new Matrix3().scale(0.5, 0.5),
+      },
+    ],
+  },
+  {
+    order: 5,
+    initialPolygons: [createSquare("square1", "blue", [-0.5, -0.5], [1, 1])],
+    objectivePolygons: [
+      applyMatrixToSquare(
+        [
+          new Matrix3().scale(2, 2),
+          new Matrix3().translate(0.5, 0.5).transpose(),
+        ],
+        createSquare("square1", "blue", [-0.5, -0.5], [1, 1])
+      ),
+    ],
+    matricesOptions: [
+      {
+        id: "translate",
+        matrix: new Matrix3().translate(0.5, 0.5).transpose(),
+      },
+      {
+        id: "scale",
+        matrix: new Matrix3().scale(2, 2),
+      },
+      {
+        id: "translate-to-origin",
+        matrix: new Matrix3().translate(-0.5, -0.5).transpose(),
       },
     ],
   },
